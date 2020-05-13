f983b2a0abd79905864886557389e34628949cc1
AVISPATEL <37960105+AVISPATEL@users.noreply.github.com>
2020-05-14 00:49:41 +0530

    Create DonorTemp.java

---
+++ src/main/java/com/avi/bloodbank/entity/DonorTemp.java
@@ -0,0 +1,88 @@
+package com.avi.bloodbank.entity;
+
+
+public class DonorTemp {
+	private int id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String country;
+	
+	private String contact;
+	
+	private String blood_group;
+	
+	private String email;
+	
+	private String pwd;
+	
+	public DonorTemp() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getBlood_group() {
+		return blood_group;
+	}
+
+	public void setBlood_group(String blood_group) {
+		this.blood_group = blood_group;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+}
