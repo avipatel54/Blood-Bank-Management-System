5c2c3bc8bef93aa04c58861c0d93286e9c491937
AVISPATEL <37960105+AVISPATEL@users.noreply.github.com>
2020-05-14 00:49:48 +0530

    Create OtpStore.java

---
+++ src/main/java/com/avi/bloodbank/entity/OtpStore.java
@@ -0,0 +1,16 @@
+package com.avi.bloodbank.entity;
+
+public class OtpStore {
+	private static int otpstore;
+
+	public static int getOtpstore() {
+		return otpstore;
+	}
+
+	public static void setOtpstore(int otpstore1) {
+		otpstore = otpstore1;
+	}
+
+	
+	
+}
